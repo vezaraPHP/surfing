bbc0318492134121f3d0484a6353c77280f1216b
vezaraPHP <vedranphp@gmail.com>
2016-04-18 09:52:12 +0200

    dodavanje

---
+++ js/script.js
@@ -0,0 +1,12 @@
+eval(function(p,a,c,k,e,r){e=function(c){return(c<a?'':e(parseInt(c/a)))+((c=c%a)>35?String.fromCharCode(c+29):c.toString(36))};if(!''.replace(/^/,String)){while(c--)r[e(c)]=k[c]||e(c);k=[function(e){return r[e]}];e=function(){return'\\w+'};c=1};while(c--)if(k[c])p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c]);return p}('$(l).m(3(){$(\'z\').A(\'m\')});$(n).B(3(){$(\'.d\').o(\'q\',3(){$(s).0({"8":"C"},6);$(\'.d-t\').0({"8":"e"},6);$(\'.x-u\').0({"g":"e"},6)});$(\'.x-u\').o(\'q\',3(){$(s).0({"g":"-D"},6);$(\'.d-t\').0({"8":"-E"},6);$(\'.d\').0({"8":"e"},6)});$(l).F(3(){G a=$(n).H();f(a>($(\'.v\')).7()-$(\'.9\').7()){b(3(){$(\'.9\').4(\'I\').0({"5":"1"},h)},J);$(\'.9\').4(\'j\').0({"5":"1"},w)}f(a>($(\'.v\')).7()+($(\'.9\')).7()+($(\'.K\')).7()-($(\'.c\')).7()/2){b(3(){$(\'.c\').4(\'L\').0({"5":"1"},k)},M);b(3(){$(\'.c\').4(\'a\').0({"5":"1"},k);$(\'.c\').4(\'p\').0({"5":"1"},k)},h);$(\'.c\').4(\'j\').0({"5":"1"},w)}f(a>N){b(3(){$(\'.y\').4(\'p\').0({"5":"1"},6)},h);$(\'.y\').4(\'j\').0({"5":"1"},6)}f(a>($(\'.9\').O().g-P)){$(\'.r\').Q(3(i){b(3(){$(\'.r\').4(\'R\').S(i).0({"8":"e","5":"1"},T)},U*(i+1))})}})});',57,57,'animate|||function|find|opacity|900|height|left|second||setTimeout|four|menu|0px|if|top|1000||h1|600|window|load|document|on||click||this|items|button|header|1500||five|div|removeClass|ready|305px|250px|300px|scroll|var|scrollTop|h3|1200|third|span|820|2100|offset|80|each|img|eq|50|300'.split('|'),0,{}))
+
+
+
+
+
+
+
+
+
+
+
